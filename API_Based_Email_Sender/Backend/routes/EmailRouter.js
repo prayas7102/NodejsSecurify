5d80141d6f37705fa55a3c3e582a8835f9517842
prayas7102 <prayas.prithvirajpratap7@gmail.com>
2023-07-24 15:14:15 +0530

    gitignore

--- API_Based_Email_Sender/Backend/routes/EmailRouter.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { SendEmail, GetEmail } = require("../controller/EmailController")
-
-router.route('/send-email').post(SendEmail);
-router.route('/get-email').get(GetEmail);
-
-module.exports = router;
