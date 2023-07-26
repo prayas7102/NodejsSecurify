b31bba1ec49fed8c8d5857fc0e9ec4d52ee8b242
prayas7102 <prayas.prithvirajpratap7@gmail.com>
2023-07-26 10:23:28 +0530

    callback hell vulnerabiltiy

--- dist/NodeJsSecurify.js
+++ dist/NodeJsSecurify.js
@@ -54,8 +54,7 @@ class Log {
         }
         try {
             // testing command:
-            // C:\Users\hp\Desktop\NodeSecurify\API_Based_Email_Sender
-            __dirname = "C:/Users/hp/Desktop/NodeSecurify/API_Based_Email_Sender";
+            __dirname = "C:/Users/hp/Desktop/NodeSecurify/TestFolder";
             // __dirname = 'D:\ChatApp\node_modules\node-js-securify\dist';
             // process.exit(1);
             console.log("\n************************************************\n".green);
--- src/NodeJsSecurify.ts
+++ src/NodeJsSecurify.ts
@@ -36,8 +36,7 @@ export class Log {
 
         try {
             // testing command:
-            // C:\Users\hp\Desktop\NodeSecurify\API_Based_Email_Sender
-            __dirname = "C:/Users/hp/Desktop/NodeSecurify/API_Based_Email_Sender"
+            __dirname = "C:/Users/hp/Desktop/NodeSecurify/TestFolder"
             // __dirname = 'D:\ChatApp\node_modules\node-js-securify\dist';
             // process.exit(1);
 
