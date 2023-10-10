6371e5da7a44b6185887590b15de77bb0f068d1b
prayas7102 <prayas.prithvirajpratap7@gmail.com>
2023-10-10 17:21:38 +0530

    dist added to gitignore

--- dist/NodeJsSecurify.d.ts
+++
@@ -1,5 +0,0 @@
-export declare class Log {
-    static NodeJsSecurifyResults(): void;
-    static findGitIgnoreFiles(directory: string): string;
-    static parseJSFiles(directory: string, gitIgnoreFilesArray: string[]): Promise<null | undefined>;
-}
