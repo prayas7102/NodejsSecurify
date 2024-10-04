0b47b01a1d642b08549458009493332ba6dce61c
eleensmathew <eleensmathew@gmail.com>
2024-10-04 22:25:10 +0530

    added pdf functionality

---
+++ src/GenerateReport.ts
@@ -0,0 +1,54 @@
+import * as fs from 'fs';
+import PDFDocument = require('pdfkit');
+
+function generatePDFReport(): void {
+    const doc = new PDFDocument();
+    const output = fs.createWriteStream('NodeJsSecurifyReport.pdf');
+
+    doc.pipe(output);
+
+    doc.fontSize(15).text('NodeJsSecurify Report', { align: 'center' });
+    doc.moveDown();
+
+    const logContent: string = fs.readFileSync('consoleOutput.log', 'utf8');
+    function parseAnsiToPdf(doc: PDFKit.PDFDocument, content: string) {
+        const lines = content.split('\n');
+        lines.forEach(line => {
+            const regex = /\x1b\[(\d+)m/g;
+            let match;
+            let lastIndex = 0;
+            while ((match = regex.exec(line)) !== null) {
+                const colorCode = parseInt(match[1], 10);
+                const text = line.substring(lastIndex, match.index);
+                if (text) {
+                    doc.fontSize(10).text(text, { continued: true });
+                }
+                switch (colorCode) {
+                    case 30: doc.fillColor('black'); break;
+                    case 31: doc.fillColor('red'); break;
+                    case 32: doc.fillColor('green'); break;
+                    case 33: doc.fillColor('yellow'); break;
+                    case 34: doc.fillColor('blue'); break;
+                    case 35: doc.fillColor('magenta'); break;
+                    case 36: doc.fillColor('cyan'); break;
+                    case 37: doc.fillColor('white'); break;
+                    case 39: doc.fillColor('black'); break; // Reset to default color
+                    default: break;
+                }
+                lastIndex = regex.lastIndex;
+            }
+            const remainingText = line.substring(lastIndex);
+            if (remainingText) {
+                doc.text(remainingText);
+            }
+            doc.text('\n');
+        });
+    }
+
+    parseAnsiToPdf(doc, logContent);
+
+
+    doc.end();
+}
+
+generatePDFReport();
